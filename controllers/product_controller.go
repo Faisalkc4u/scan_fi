0227314a2eab28c17f1c229fd1875379f9e03843
Faisal <faisal@mvp-apps.ae>
2025-08-03 14:00:59 +0400

    Change error response for product not found to Bad Request status

--- controllers/product_controller.go
+++ controllers/product_controller.go
@@ -61,7 +61,7 @@ func GetProductByID(c *gin.Context) {
 	id := c.Param("id")
 	var product models.Product
 	if err := config.DB.First(&product, id).Error; err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"message": "Product not found"})
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Product not found"})
 		return
 	}
 	c.JSON(http.StatusOK, product)
@@ -71,7 +71,7 @@ func UpdateProduct(c *gin.Context) {
 	id := c.Param("id")
 	var product models.Product
 	if err := config.DB.First(&product, id).Error; err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"message": "Product not found"})
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Product not found"})
 		return
 	}
 
@@ -88,7 +88,7 @@ func DeleteProduct(c *gin.Context) {
 	id := c.Param("id")
 	var product models.Product
 	if err := config.DB.First(&product, id).Error; err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"message": "Product not found"})
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Product not found"})
 		return
 	}
 
